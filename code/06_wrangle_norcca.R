71aa307c4aecfe7974d6b432fdb1fa76f5fda830
Torstensson Anders <a002440@ad.smhi.se>
2024-01-28 00:33:24 +0100

    Initial commit

---
+++ code/06_wrangle_norcca.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+
+# Read NORCCA taxa lists
+norcca <- read_tsv("data_in/norcca_extended.txt") # Full list with links
+norcca_worms <- read_tsv("data_in/norcca_extended_taxa_names_matched.txt") # List that has been matched with the WoRMS web match interface
+
+norcca_worms <- norcca_worms %>%
+  rename(scientific_name = ScientificName...1,
+         ScientificName = ScientificName...4,
+         status = `Taxon status`) %>%
+  select(scientific_name, status, AphiaID, AphiaID_accepted)
+
+# Read taxa_worms file
+taxa_worms <- read_tsv("data_in/taxa_worms.txt")
+
+# Join tables
+norcca_combined <- norcca %>%
+  left_join(norcca_worms) %>%
+  mutate(used_aphia_id = ifelse(status == "unaccepted", AphiaID_accepted, AphiaID),
+         scientific_name = gsub(" sp.", "", scientific_name)) %>%
+  select(-status, -AphiaID, -AphiaID_accepted, -class, -environment) %>%
+  rename(AphiaID = used_aphia_id) %>%
+  filter(AphiaID %in% taxa_worms$aphia_id)
+
+# Store file
+write_delim(norcca_combined, "data_out/norcca.txt", delim = "\t") 
