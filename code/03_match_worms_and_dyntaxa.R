53fc13cfe450881abbc565c9bb494a2e72da4945
Torstensson Anders <a002440@ad.smhi.se>
2024-02-26 18:53:40 +0100

    small update

--- code/03_match_worms_and_dyntaxa.R
+++ code/03_match_worms_and_dyntaxa.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 # Read taxa_worms file
-taxa_worms <- read_tsv("data_in/taxa_worms.txt")
+taxa_worms <- read_tsv("data_out/taxa_worms_accepted.txt")
 
 # Match taxa_worms with Dyntaxa through the web match interface, read .txt-file here
 dyntaxa_records <- read.table("data_in/dyntaxa_match.txt", header=TRUE, sep="\t", fill = TRUE, quote = "")
@@ -20,7 +20,9 @@ dyntaxa_list <- taxa_worms %>%
   select(aphia_id, scientific_name) %>%
   distinct() %>%
   right_join(dyntaxa_list) %>%
-  rename(AphiaID = aphia_id)
+  rename(AphiaID = aphia_id) %>%
+  filter(!is.na(AphiaID)) %>%
+  rename(taxon_id = AphiaID)
 
 # Store file
-write_delim(dyntaxa_list, "data_out/worms_to_dyntaxa.txt", delim = "\t") 
+write_delim(dyntaxa_list, "data_out/facts_external_links_dyntaxa.txt", delim = "\t") 
