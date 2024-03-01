9222022e80835a3effd375337fd282bb893f05dd
Torstensson Anders <a002440@ad.smhi.se>
2024-03-01 21:53:11 +0100

    Cleanup

--- code/05_wrangle_norcca.R
+++ code/05_wrangle_norcca.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 
 # Read NORCCA taxa lists
-# norcca <- read_tsv("data_in/norcca_extended.txt") # Full list with links
 norcca_worms <- read_tsv("data_in/norcca_extended_taxa_names_matched.txt") # List that has been matched with the WoRMS web match interface
 norcca_strains <- read_tsv("data_in/norcca_strains.txt") # List that has been matched with the WoRMS web match interface
+taxa_worms <- read_tsv("data_out/content/taxa.txt")
 
 # Fix column names and select relevant information
 norcca_worms <- norcca_worms %>%
@@ -11,11 +11,6 @@ norcca_worms <- norcca_worms %>%
          ScientificName = ScientificName...4,
          status = `Taxon status`) %>%
   select(scientific_name, status, AphiaID, AphiaID_accepted)
-
-# Read taxa_worms file
-taxa_worms <- read_tsv("data_out/content/taxa.txt")
-
-
 
 # Join tables
 norcca_combined <- norcca_strains %>%
@@ -27,24 +22,5 @@ norcca_combined <- norcca_strains %>%
   rename(taxon_id = used_aphia_id) %>%
   filter(taxon_id %in% taxa_worms$taxon_id)
 
-
-
-# # Join tables
-# norcca_combined <- norcca %>%
-#   left_join(norcca_worms) %>%
-#   mutate(used_aphia_id = ifelse(status == "unaccepted", AphiaID_accepted, AphiaID),
-#          scientific_name = gsub(" sp.", "", scientific_name)) %>%
-#   select(-status, -AphiaID, -AphiaID_accepted, -class, -environment) %>%
-#   rename(AphiaID = used_aphia_id) %>%
-#   filter(AphiaID %in% taxa_worms$aphia_id) %>%
-#   rename("Link to NORCCA website" = taxa_link)
-
-# # Version with only links to species, not all strains
-# norcca_short <- norcca_combined %>%
-#   select(-strain, -strain_link) %>%
-#   distinct() %>%
-#   rename("Link to NORCCA website" = taxa_link)
- 
 # Store file
-write_delim(norcca_combined, "data_out/content/facts_external_links_norcca.txt", delim = "\t", na = "")
-# write_delim(norcca_short, "data_out/norcca.txt", delim = "\t") 
+write_tsv(norcca_combined, "data_out/content/facts_external_links_norcca.txt", na = "")
